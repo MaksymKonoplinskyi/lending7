363d606588dc30d7700542e5da252e705f770641
MaksymKonoplinskyi <89479253+MaksymKonoplinskyi@users.noreply.github.com>
2022-07-01 20:46:28 +0300

    5

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header_nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLinks = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav_active');
 
@@ -22,46 +23,54 @@
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header__nav_active');
 
-    })
-}())
+    });
+    if (window.innerWidth <= 767) {
+        for (let i = 0; i < menuLinks.length; i += 1) {
+            menuLinks[i].addEventListener('click', () => {
+                menu.classList.remove('header__nav_active')
+
+            })
+        }
+    }
+}());
 
 
-    // Scroll to anchors
-    (function () {
+// Scroll to anchors
+(function () {
 
-        const smoothScroll = function (targetEl, duration) {
-            const headerElHeight = document.querySelector('.header').clientHeight;
-            let target = document.querySelector(targetEl);
-            let targetPosition = target.getBoundingClientRect().top - headerElHeight;
-            let startPosition = window.pageYOffset;
-            let startTime = null;
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight = document.querySelector('.header').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
 
-            const ease = function (t, b, c, d) {
-                t /= d / 2;
-                if (t < 1) return c / 2 * t * t + b;
-                t--;
-                return -c / 2 * (t * (t - 2) - 1) + b;
-            };
-
-            const animation = function (currentTime) {
-                if (startTime === null) startTime = currentTime;
-                const timeElapsed = currentTime - startTime;
-                const run = ease(timeElapsed, startPosition, targetPosition, duration);
-                window.scrollTo(0, run);
-                if (timeElapsed < duration) requestAnimationFrame(animation);
-            };
-            requestAnimationFrame(animation);
-
+        const ease = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
         };
 
-        const scrollTo = function () {
-            const links = document.querySelectorAll('.js-scroll');
-            links.forEach(each => {
-                each.addEventListener('click', function () {
-                    const currentTarget = this.getAttribute('href');
-                    smoothScroll(currentTarget, 1000);
-                });
+        const animation = function (currentTime) {
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0, run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
             });
-        };
-        scrollTo();
-    }());
+        });
+    };
+    scrollTo();
+}());
